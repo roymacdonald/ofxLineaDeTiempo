de5f0d53d035e2af8e45181563cd7dcc99834130
Roy Macdonald <macdonald.roy@gmail.com>
2020-03-19 00:56:14 -0300

    src/ofxLineaDeTiempo.* changes

--- src/ofxLineaDeTiempo.h
+++ src/ofxLineaDeTiempo.h
@@ -2,30 +2,32 @@
 
 #include "ofMain.h"
 #include "MUI.h"
-#include "TracksPanel.h"
+#include "LineaDeTiempo/Controller/TracksPanelController.h"
 
-class ofxLineaDeTiempo: public ofx::MUI::MUI{
-public:
-	
-	ofxLineaDeTiempo();
-	
-    void setup();
-    void draw();
-	void update();
-	
-	void keyReleased(ofKeyEventArgs& args);
+using ofxLineaDeTiempo = ofx::LineaDeTiempo::TracksPanelController;
 
-	
-	virtual void onDraw() const override;
-	
-
-	ofx::LineaDeTiempo::TracksPanel * tr = nullptr;
-	
-	ofx::LineaDeTiempo::KeyFramesTrack* t1 = nullptr;
-	
-	
-	
-	
-	ofEventListener keyListener;
-	
-};
+//public ofx::MUI::MUI{
+//public:
+//
+//	ofxLineaDeTiempo();
+//
+//    void setup();
+//    void draw();
+//	void update();
+//
+//	void keyReleased(ofKeyEventArgs& args);
+//
+//
+//	virtual void onDraw() const override;
+//
+//
+//	ofx::LineaDeTiempo::TracksPanelController * tr = nullptr;
+//
+////	ofx::LineaDeTiempo::KeyFramesTrack* t1 = nullptr;
+//
+//
+//
+//
+//	ofEventListener keyListener;
+//
+//};
