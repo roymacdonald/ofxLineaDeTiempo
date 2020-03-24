d2648017620e5f49ca3089e35ce987dbea0d8902
Roy Macdonald <macdonald.roy@gmail.com>
2020-03-23 23:49:01 -0300

    Added is_multi_dim_param type trait

--- libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/Utils/ofxTypeTraits.h
+++ libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/Utils/ofxTypeTraits.h
@@ -24,3 +24,18 @@ struct is_ofColor
 		std::is_same<ofColor_<float>          , typename std::remove_cv<T>::type>::value
 
 > {};
+
+
+template< class T >
+struct is_multi_dim_param
+     : std::integral_constant<
+         bool,
+		is_ofColor<T>::value ||
+
+		std::is_same<glm::vec2  , typename std::remove_cv<T>::type>::value  ||
+		std::is_same<glm::vec3  , typename std::remove_cv<T>::type>::value  ||
+		std::is_same<glm::vec4  , typename std::remove_cv<T>::type>::value  ||
+		std::is_same<glm::quat  , typename std::remove_cv<T>::type>::value  ||
+		std::is_same<glm::mat4  , typename std::remove_cv<T>::type>::value
+
+> {};
