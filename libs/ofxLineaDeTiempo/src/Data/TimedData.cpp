930383e85051f95ca256fca3b382f1ea709f6df3
Roy Macdonald <macdonald.roy@gmail.com>
2020-03-24 19:37:52 -0300

    Cleanup of KeyframedData

--- libs/ofxLineaDeTiempo/src/Data/TimedData.cpp
+++ libs/ofxLineaDeTiempo/src/Data/TimedData.cpp
@@ -28,23 +28,23 @@ void TimedData_<DataType>::toJson(ofJson& j)
 	j["time"] = this->time;
 }
 
-template<typename DataType>
-void TimedData_<DataType>::setTime(uint64_t _time)
-{
-		
-#ifdef USE_EVENTS_FOR_TIMED_DATA_CHANGE
-#ifdef COMPARE_TIME_ON_SETTER
-	if(time == _time) return;
-#endif
-	ofNotifyEvent(timeChangedEvent, _time, this);
-#endif
-	time = _time;
-}
-template<typename DataType>
-const uint64_t& TimedData_<DataType>::getTime() const
-{
-	return time;
-}
+//template<typename DataType>
+//void TimedData_<DataType>::setTime(uint64_t _time)
+//{
+//
+//#ifdef USE_EVENTS_FOR_TIMED_DATA_CHANGE
+//#ifdef COMPARE_TIME_ON_SETTER
+//	if(time == _time) return;
+//#endif
+//	ofNotifyEvent(timeChangedEvent, _time, this);
+//#endif
+//	time = _time;
+//}
+//template<typename DataType>
+//const uint64_t& TimedData_<DataType>::getTime() const
+//{
+//	return time;
+//}
 
 
 
