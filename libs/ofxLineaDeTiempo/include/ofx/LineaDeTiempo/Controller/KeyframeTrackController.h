b112bb665ee50a877a3916bd109b2632e382dfe1
Roy Macdonald <macdonald.roy@gmail.com>
2020-03-26 15:57:38 -0300

    Implemented KeyframeTrackController. working with views and all. Updating needs to be implemented yet

--- libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/Controller/KeyframeTrackController.h
+++ libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/Controller/KeyframeTrackController.h
@@ -1,5 +1,5 @@
 //
-//  KeyFrameTrackController.hpp
+//  KeyframeTrackController.hpp
 //  tracksAndTimeTest
 //
 //  Created by Roy Macdonald on 3/17/20.
@@ -8,8 +8,10 @@
 #pragma once
 
 #include "LineaDeTiempo/Controller/TrackController.h"
-#include "LineaDeTiempo/Controller/KeyFrameRegionController.h"
+#include "LineaDeTiempo/Controller/KeyframeRegionController.h"
+#include "LineaDeTiempo/Data/KeyframedData.h"
 
+#include "ofParameter.h"
 
 namespace ofx {
 namespace LineaDeTiempo {
@@ -17,22 +19,45 @@ namespace LineaDeTiempo {
 //parent type forward declaration
 class TrackGroupController;
 
-
-class KeyFrameTrackController
+template <typename DataType>
+class KeyframeTrackController_
 : public TrackController
 {
 public:
 		
-	KeyFrameTrackController(const std::string& name, TrackGroupController* parent, TimeControl* timeControl);
 	
-	virtual ~KeyFrameTrackController() = default;
+	KeyframeTrackController_(ofParameter<DataType>& parameter, TrackGroupController* parent, TimeControl* timeControl);
 	
-	KeyFrameRegionController * addRegion( const std::string& regionName, const ofRange64u& timeRange);
+	KeyframeTrackController_(const std::string& name, TrackGroupController* parent, TimeControl* timeControl);
 	
-	bool removeRegion(KeyFrameRegionController* track);
+	virtual ~KeyframeTrackController_() = default;
+	
+	KeyframeRegionController_<DataType>* addRegion( const std::string& regionName, const ofRange64u& timeRange);
+	
+	bool removeRegion(KeyframeRegionController_<DataType>* track);
+	
+	virtual void generateView() override;
+	virtual void destroyView() override;
 	
 	
+	virtual void update(uint64_t& t) override;
+	ofParameter<DataType>& getParameter();
+	const ofParameter<DataType>& getParameter() const;
+	
+	
+	float getNormalizedValue(const DataType& val);
+	DataType getUnnormalizedValue(float val);
 protected:
+	ofParameter<DataType> _parameter;
+	
+
+	ofEventListener _paramListener;
+	
+	void _paramChanged(DataType& );
+	
+	//	// this is set to true when the param is being modified by the manager, so it does not generate an infinite loop because of the cyclic callbacks.
+	bool _bModifyingParam = false;
+
 	
 	
 	
@@ -44,3 +69,4 @@ private:
 
 
 } } // ofx::LineaDeTiempo
+
--- libs/ofxLineaDeTiempo/src/Controller/KeyframeTrackController.cpp
+++ libs/ofxLineaDeTiempo/src/Controller/KeyframeTrackController.cpp
@@ -1,35 +1,176 @@
 //
-//  KeyFrameTrackController.cpp
+//  KeyframeTrackController.cpp
 //  tracksAndTimeTest
 //
 //  Created by Roy Macdonald on 3/17/20.
 //
 
-#include "KeyFrameTrackController.h"
+#include "LineaDeTiempo/Controller/KeyframeTrackController.h"
 
 #include "LineaDeTiempo/Controller/TrackGroupController.h"
-#include "LineaDeTiempo/Controller/KeyFrameRegionController.h"
+
+#include "LineaDeTiempo/Utils/ofxTypeTraits.h"
+//#include "LineaDeTiempo/View/MultiDimTrackView.h"
 
 
 namespace ofx {
 namespace LineaDeTiempo {
-
-
-KeyFrameTrackController::KeyFrameTrackController(const std::string& name, TrackGroupController* parent, TimeControl* timeControl)
-:TrackController(name, parent, timeControl)
+//
+template <typename DataType>
+KeyframeTrackController_<DataType>::KeyframeTrackController_(ofParameter<DataType>& parameter,TrackGroupController* parent, TimeControl* timeControl)
+: KeyframeTrackController_<DataType>(parameter.getName(), parent, timeControl)
 {
+	_parameter.makeReferenceTo(parameter);
+}
+
+template <typename DataType>
+KeyframeTrackController_<DataType>::KeyframeTrackController_(const std::string& name, TrackGroupController* parent, TimeControl* timeControl)
+: TrackController(name, parent, timeControl)
+{
+	_parameter.setName(name);
 	
+	_paramListener = _parameter.newListener(this, &KeyframeTrackController_<DataType>::_paramChanged);
+	enableTimeUpdate();
 }
 
-KeyFrameRegionController * KeyFrameTrackController::addRegion( const std::string& regionName, const ofRange64u& timeRange)
+template <typename DataType>
+KeyframeRegionController_<DataType> * KeyframeTrackController_<DataType>::addRegion( const std::string& regionName, const ofRange64u& timeRange)
 {
-	return _addRegion<KeyFrameRegionController>(regionName, timeRange);
+	return _addRegion<KeyframeRegionController_<DataType>>( regionName, timeRange);
 }
 
-bool KeyFrameTrackController::removeRegion(KeyFrameRegionController* track)
+template <typename DataType>
+bool KeyframeTrackController_<DataType>::removeRegion(KeyframeRegionController_<DataType>* track)
 {
 	return _removeRegion(track);
 }
 
+template <typename DataType>
+ofParameter<DataType>& KeyframeTrackController_<DataType>::getParameter()
+{
+	return _parameter;
+}
+
+template <typename DataType>
+const ofParameter<DataType>& KeyframeTrackController_<DataType>::getParameter() const
+{
+	return _parameter;
+}
+template <typename DataType>
+void KeyframeTrackController_<DataType>::update(uint64_t& time)
+{
+//	if(_keyframedData.update(time))
+//	{
+//		_bModifyingParam = true;
+//		auto v = _keyframedData.getCurrentValue();
+//		if(_parameter.get() != v)// avoid setting the parameter constantly as this will trigger the parameters events.
+//		{
+//
+//			_parameter = v;
+//		}
+//
+//		_bModifyingParam = false;
+//	}
+}
+
+
+template <typename DataType>
+void KeyframeTrackController_<DataType>::_paramChanged(DataType& ){
+//	if(!_bModifyingParam)
+//	{
+//		_keyframedData.setValue(_parameter, getTimeControl()->getCurrentTime(), _keyframedData.isKeyFramingEnabled() );
+//	}
+}
+
+template <typename DataType>
+void KeyframeTrackController_<DataType>::generateView()
+{
+
+	if(is_multi_dim_param<DataType>::value)
+	{
+//		_generateView<MultiDimTrackView>();
+	}
+	else
+	{
+		TrackController::generateView();
+	}
+
+
+
+}
+
+template <typename DataType>
+void KeyframeTrackController_<DataType>::destroyView()
+{
+	if(is_multi_dim_param<DataType>::value)
+	{
+
+	}
+	else
+	{
+		TrackController::destroyView();
+	}
+
+
+}
+
+template <typename DataType>
+DataType KeyframeTrackController_<DataType>::getUnnormalizedValue(float val)
+{
+return ofMap((float)val, 0, 1, (float)_parameter.getMin(), (float)_parameter.getMax(), true);
+}
+
+template <typename DataType>
+float KeyframeTrackController_<DataType>::getNormalizedValue(const DataType& val)
+{
+	return ofMap((float)val, (float)_parameter.getMin(), (float)_parameter.getMax(), 0, 1, true);
+}
+
 } } // ofx::LineaDeTiempo
+
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofRectangle>;
+
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofColor_<char>>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofColor_<unsigned char>>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofColor_<short>>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofColor_<unsigned short>>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofColor_<int>>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofColor_<unsigned int>>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofColor_<long>>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofColor_<unsigned long>>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofColor_<float>>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofColor_<double>>;
+
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<glm::vec2>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<glm::vec3>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<glm::vec4>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<glm::quat>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<glm::mat4>;
+//
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofVec2f>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofVec3f>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<ofVec4f>;
+
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<bool>;
+//template class ofx::LineaDeTiempo::KeyframeTrackController_<void>;
+
+template class ofx::LineaDeTiempo::KeyframeTrackController_<char>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<unsigned char>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<signed char>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<short>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<unsigned short>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<int>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<unsigned int>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<long>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<unsigned long>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<long long>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<unsigned long long>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<float>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<double>;
+template class ofx::LineaDeTiempo::KeyframeTrackController_<long double>;
+
+
+
+
+
 
