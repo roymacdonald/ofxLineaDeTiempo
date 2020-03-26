7c179d1cbff1b485b8fa15caaf2fc105046e0042
Roy Macdonald <macdonald.roy@gmail.com>
2020-03-26 15:56:05 -0300

    Several improvements to KeyframeRegionController. now it all works properly

--- libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/Controller/KeyframeRegionController.h
+++ libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/Controller/KeyframeRegionController.h
@@ -1,5 +1,5 @@
 //
-//  KeyFrameRegionController.hpp
+//  KeyframeRegionController.hpp
 //  tracksAndTimeTest
 //
 //  Created by Roy Macdonald on 3/17/20.
@@ -7,34 +7,71 @@
 
 #pragma once
 #include "LineaDeTiempo/Controller/RegionController.h"
+#include "LineaDeTiempo/Controller/KeyframeController.h"
+#include "LineaDeTiempo/View/KeyframesRegionView.h"
+#include "LineaDeTiempo/View/KeyframeView.h"
 
+#include "LineaDeTiempo/Data/KeyframedData.h"
+#include <unordered_map>
 namespace ofx {
 namespace LineaDeTiempo {
 
+template<typename DataType>
+class KeyframeTrackController_;
 
 class TrackController;
 
 
-class KeyFrameRegionController
+template<typename DataType>
+class KeyframeRegionController_
 : public RegionController
 
 
 {
 public:
 
-	KeyFrameRegionController(const std::string& name, const ofRange64u& timeRange, TrackController* parentTrack, TimeControl* timeControl);
+	KeyframeRegionController_( const std::string& name, const ofRange64u& timeRange, TrackController* parentTrack, TimeControl* timeControl);
 	
-	virtual ~KeyFrameRegionController() = default;
+	virtual ~KeyframeRegionController_() = default;
 	
 	virtual void generateView() override;
 	virtual void destroyView() override;
 	
+	using  BaseViewController<RegionView>::getView;
+	using  BaseViewController<RegionView>::setView;
 	
+	
+	KeyframeController<DataType>* addKeyframe(DataType value, uint64_t time);
+	
+	bool removeKeyframe(KeyframeController<DataType>* keyframe);
+	
+	
+	const KeyframedData_<DataType>& getKeyframedData() const;
 	
 protected:
 
+	friend class KeyframeTrackController_< DataType >;
+	friend class KeyframeController<DataType>;
+
+	
+	ofEventListener addKeyframeListener;
+	void _addKeyframeAtScreenPos(glm::vec2& pos);
+	
+	ofEventListener removeKeyframeListener;
+	void _removeKeyframe(KeyframeView*& v);
+	
+	KeyframedData_<DataType> _keyframedData;
+
+	std::vector<KeyframeController<DataType>* > _keyframes;
+	std::unordered_map<KeyframeView*, KeyframeController<DataType>*>_keyframesViewMap;
+	
+	
+	KeyframeTrackController_<DataType>* _parentTrack = nullptr;
+	
 private:
 	
+	
+	KeyframesRegionView* _keyframesRegionView = nullptr;
 	
 };
 
--- libs/ofxLineaDeTiempo/src/Controller/KeyframeRegionController.cpp
+++ libs/ofxLineaDeTiempo/src/Controller/KeyframeRegionController.cpp
@@ -1,61 +1,215 @@
 //
-//  KeyFrameRegionController.cpp
+//  KeyframeRegionController.cpp
 //  tracksAndTimeTest
 //
 //  Created by Roy Macdonald on 3/17/20.
 //
 
-#include "LineaDeTiempo/Controller/KeyFrameRegionController.h"
-#include "LineaDeTiempo/Controller/TrackController.h"
+#include "LineaDeTiempo/Controller/KeyframeRegionController.h"
+#include "LineaDeTiempo/Controller/KeyframeTrackController.h"
 #include "LineaDeTiempo/View/KeyframesRegionView.h"
+
 namespace ofx {
 namespace LineaDeTiempo {
 
 
-
-KeyFrameRegionController::KeyFrameRegionController(const std::string& name, const ofRange64u& timeRange, TrackController* parentTrack, TimeControl* timeControl)
+template<typename T>
+KeyframeRegionController_<T>::KeyframeRegionController_(const std::string& name, const ofRange64u& timeRange, TrackController* parentTrack, TimeControl* timeControl)
 : RegionController(name, timeRange, parentTrack, timeControl)
+, _parentTrack(dynamic_cast<KeyframeTrackController_<T>*>(parentTrack))
 {
+
 	
 }
 
-
-
-void KeyFrameRegionController::generateView()
+template<typename T>
+void KeyframeRegionController_<T>::generateView()
 {
 	if(getView() == nullptr){
 	auto p = dynamic_cast<TrackController*>(parent());
 	if(p && p->getView())
 	{
+		_keyframesRegionView = p->getView()->addRegion<KeyframesRegionView, KeyframeRegionController_<T>>(this);
 		
-		setView(p->getView()->addRegion<KeyframesRegionView>(this));
-			
+		setView(_keyframesRegionView);
+		
+		
+		addKeyframeListener = _keyframesRegionView->addKeyframeEvent.newListener(this,&KeyframeRegionController_<T>::_addKeyframeAtScreenPos);
+		
+		removeKeyframeListener = _keyframesRegionView->removeKeyframeEvent.newListener(this, &KeyframeRegionController_<T>::_removeKeyframe);
+		
+		
+//		keyframeAddedListener = r->keyframeAddedEvent.newListener(this, &KeyframeRegionController_<T>::_keyframeAdded);
+//
+//		keyframeRemovedListener = r->keyframeRemovedEvent.newListener(this, &KeyframeRegionController_<T>::_keyframeRemoved);
+//
 	}
 
-	generateChildrenViews(this);
+		generateChildrenViews(this);
+		_keyframesViewMap.clear();
+		for(auto k: _keyframes)
+		{
+			if(k && k->getView())
+			{
+				_keyframesViewMap[k->getView()] = k;
+			}
+		}
 	}
 }
-void KeyFrameRegionController::destroyView()
+template<typename T>
+void KeyframeRegionController_<T>::destroyView()
 {
 	if(getView()){
 	destroyChildrenViews(this);
-	
+	_keyframesViewMap.clear();
+		
 	auto p = dynamic_cast<TrackController*>(parent());
 	if(p && p->getView())
 	{
 		
 		if(p->getView()->removeRegion(this) == false)
 		{
-			ofLogError("KeyFrameRegionController::destroyView") << "Could not remove track correctly. " << getId();
+			ofLogError("KeyframeRegionController_<T>::destroyView") << "Could not remove track correctly. " << getId();
 		}
-		
+		addKeyframeListener.unsubscribe();
+		removeKeyframeListener.unsubscribe();
 		setView(nullptr);
+		_keyframesRegionView = nullptr;
 	}
 	}
 }
 
+template<typename T>
+KeyframeController<T>* KeyframeRegionController_<T>::addKeyframe(T value, uint64_t time)
+{
+	auto d = _keyframedData.add(value, time);
+	
+	auto k = addChild<KeyframeController<T>>( "k_"+ofToString(_keyframedData.size()), d, this );
+	
+	_keyframes.push_back(k);
+	
+	if(getView())
+	{
+		k->generateView();
+		_keyframesViewMap[k->getView()] = k;
+	}
+	
+	return k;
+}
+template<typename T>
+bool KeyframeRegionController_<T>::removeKeyframe(KeyframeController<T>* keyframe)
+{
+	_keyframedData.remove(keyframe->getData());
+	
+	if(keyframe->getView())
+	{
+		_keyframesViewMap.erase(keyframe->getView());
+		keyframe->destroyView();
+		
+	}
+	
+	return (removeChild(keyframe) != nullptr);
+}
 
 
+
+template<typename T>
+void KeyframeRegionController_<T>::_removeKeyframe(KeyframeView*& v)
+{
+	if(!v)
+	{
+		ofLogWarning("KeyframeRegionController_<T>::_removeKeyframe") << "failed because a nullptr was passed to it. This should not happen.";
+		return;
+	}
+	if(_keyframesViewMap.count(v))
+	{
+		removeKeyframe(_keyframesViewMap[v]);
+	}
+}
+
+
+//template<typename T>
+//void KeyframeRegionController_<T>::_keyframeAdded(KeyframeView * view)
+//{
+//	if(view && _viewDataMap.count(view) == 0)
+//	{
+//		auto k = _keyframedData.add(_parentTrack->getUnnormalizedValue(view->getValue()),
+//									view->getTime());
+//
+//		if(k)
+//		{
+//			_viewDataMap[view] = k;
+//		}
+//	}
+//}
+//
+//template<typename T>
+//void KeyframeRegionController_<T>::_keyframeRemoved(KeyframeView * view)
+//{
+//	if(view && _viewDataMap.count(view))
+//	{
+//		_keyframedData.remove(_viewDataMap[view]);
+//
+//		_viewDataMap.erase(view);
+//	}
+//}
+//
+//template<typename T>
+//void KeyframeRegionController_<T>::_keyframesRemoved(std::vector<KeyframeView *>& keyframes)
+//{
+//	for(auto k : keyframes)
+//	{
+//		_keyframeRemoved(k);
+//	}
+//}
+
+template<typename T>
+void KeyframeRegionController_<T>::_addKeyframeAtScreenPos(glm::vec2& pos)
+{
+	
+	if(getView() && _parentTrack && _parentTrack->getView()){
+//		std::cout << "pos : " << pos;
+		
+		auto time = _parentTrack->getView()->screenPositionToTime(pos.x);
+		
+//		std::cout << "  time: " << time ;
+		
+		auto r = getView()->getScreenShape();
+		
+		auto & p = _parentTrack->getParameter();
+		auto value = ofMap(pos.y, r.getMinY(), r.getMaxY() - KeyframeView::defaultKeyframeSize, p.getMin(), p.getMax(), true);
+
+//		std::cout << "  value: " << value << "\n";
+		
+		this->addKeyframe(value, time);
+
+//		Esta transformacion de coordenadas no esta funcando bien.
+//		HAcer que se solo una funcion la que hace la transformacion.
+		
+	}
+}
+
+template<typename T>
+const KeyframedData_<T>& KeyframeRegionController_<T>::getKeyframedData() const
+{
+	return _keyframedData;
+}
+
+
+
+
+//template<typename T>
+//void KeyframeRegionController_<T>::updateKeyframeTimeAndValue(KeyframeView* keyframe)
+//{
+//	if(_viewDataMap.count(keyframe) && _parentTrack)
+//	{
+//		_viewDataMap[keyframe]->time = keyframe->getTime();
+//		_viewDataMap[keyframe]->value = _parentTrack->getUnnormalizedValue(keyframe->getValue());
+//	}
+//}
+
+template class KeyframeRegionController_<int>;
+template class KeyframeRegionController_<float>;
 
 } } // ofx::LineaDeTiempo
 
