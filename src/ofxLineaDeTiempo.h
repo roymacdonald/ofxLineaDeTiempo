3ad3ec526a5425c64fbd4df2a3a6ca8578fd5acf
Roy Macdonald <macdonald.roy@gmail.com>
2020-03-26 16:17:24 -0300

    removed ofxLineaDeTiempo.cpp and cleand up .h

--- src/ofxLineaDeTiempo.h
+++ src/ofxLineaDeTiempo.h
@@ -5,29 +5,3 @@
 #include "LineaDeTiempo/Controller/TracksPanelController.h"
 
 using ofxLineaDeTiempo = ofx::LineaDeTiempo::TracksPanelController;
-
-//public ofx::MUI::MUI{
-//public:
-//
-//	ofxLineaDeTiempo();
-//
-//    void setup();
-//    void draw();
-//	void update();
-//
-//	void keyReleased(ofKeyEventArgs& args);
-//
-//
-//	virtual void onDraw() const override;
-//
-//
-//	ofx::LineaDeTiempo::TracksPanelController * tr = nullptr;
-//
-////	ofx::LineaDeTiempo::KeyFramesTrack* t1 = nullptr;
-//
-//
-//
-//
-//	ofEventListener keyListener;
-//
-//};
