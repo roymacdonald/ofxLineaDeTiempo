6a1794f18a83df1faa9eb215af3db1c1e38bfdc9
Roy Macdonald <macdonald.roy@gmail.com>
2020-03-27 22:38:45 -0300

    TracksPanel. Some Cleanup and formating

--- libs/ofxLineaDeTiempo/src/View/TracksPanel.cpp
+++ libs/ofxLineaDeTiempo/src/View/TracksPanel.cpp
@@ -20,60 +20,60 @@
 
 namespace ofx {
 namespace LineaDeTiempo {
-//using namespace MUI;
+
 TracksPanel::TracksPanel(const std::string& id, DOM::Element* parentView, const ofRectangle& rect, TracksPanelController* controller)
-//const std::string& name, TrackGroupView* parentGroupView, TrackGroupController * controller
+
 : TrackGroupView(parentView, controller)
 
 {
-//	std::cout << "TrackGroupView " << id << "  " << rect << string((controller != nullptr)?controller->getId():"null controller") << "\n";
-	
-	
-//	setParent(controller->getMainView());
-//	tracksView = addChild<TracksScrollPanel>(id + "_tracksView", ofRectangle(rect.x +_trackHeaderWidth,rect.y, rect.width  - _trackHeaderWidth, rect.height ));
 	setShape(rect);
 	tracksView = DOM::Element::addChild<MUI::TracksScrollPanel>(id + "_tracksView", _makeTracksViewRect());
 	
 	tracksView->setForceShowScrollbars(true);
 	tracksView->setMoveToFrontOnCapture(false);
-//	headersView = addChild<ClippedView_<Widget>>(id + "headersView", ofRectangle(rect.x, rect.y, _trackHeaderWidth, rect.height));
+
 	headersView = addChild<MUI::ClippedView>(id + "headersView", _makeHeadersViewRect());
 	
 	_tracksContainerListeners.push(tracksView->getContainer()->move.newListener(this, &TracksPanel::_tracksMoved));
 	_tracksContainerListeners.push(tracksView->getContainer()->resize.newListener(this, &TracksPanel::_tracksResized));
 
 	_playhead = tracksView->getContainer()->addChild<Playhead>(this, controller->getTimeControl());//, _timeControl);
-//	MUI::Widget::setMoveToFrontOnCapture(false);
-//	tracksView->setScrollV({0,1});
-//	tracksView->setScrollH({0,1});
+
 }
+
 TracksClippedView* TracksPanel::getClippingView()
 {
 	return tracksView->getClippingView();
 }
+
 const TracksClippedView* TracksPanel::getClippingView()const
 {
 	return tracksView->getClippingView();
 }
+
 DOM::Element* TracksPanel::getContainer()
 {
 	return tracksView->getContainer();
 }
+
 const DOM::Element* TracksPanel::getContainer() const
 {
 	return tracksView->getContainer();
 }
+
 //---------------------------------------------------------------------
 void TracksPanel::_tracksMoved(DOM::MoveEventArgs& e)
 {
 	_updateHeadersFromTracks();
 	_playhead->updatePosition();
 }
+
 //---------------------------------------------------------------------
 void TracksPanel::_tracksResized(DOM::ResizeEventArgs& e)
 {
 	_updateHeadersFromTracks();
 }
+
 //---------------------------------------------------------------------
 void TracksPanel::_updateHeadersFromTracks()
 {
@@ -108,18 +108,21 @@ void TracksPanel::updateLayout()
 		
 	}
 }
+
 //---------------------------------------------------------------------
 ofRectangle TracksPanel::_makeHeadersViewRect()
 {
 	
 	return ofRectangle(0, CONTAINER_MARGIN, _trackHeaderWidth, getHeight() - (CONTAINER_MARGIN * 2) - SCROLL_BAR_SIZE);
 }
+
 //---------------------------------------------------------------------
 ofRectangle TracksPanel::_makeTracksViewRect()
 {
 	return ofRectangle(_trackHeaderWidth, 0, getWidth() - _trackHeaderWidth, getHeight());
 }
 	
+
 //---------------------------------------------------------------------
 float TracksPanel::timeToScreenPosition(uint64_t time) const
 {
@@ -138,6 +141,7 @@ float TracksPanel::timeToScreenPosition(uint64_t time) const
 		
 	
 }
+
 //---------------------------------------------------------------------
 uint64_t  TracksPanel::screenPositionToTime(float x) const
 {
@@ -159,15 +163,14 @@ uint64_t  TracksPanel::screenPositionToTime(float x) const
 
 	
 }
+
 //---------------------------------------------------------------------
-
-
-
 DOM::Element* TracksPanel::_getTracksContainer()
 {
 	return tracksView->getContainer();
 }
 
+//---------------------------------------------------------------------
 DOM::Element* TracksPanel::_getHeadersContainer()
 {
 	return headersView->container;
