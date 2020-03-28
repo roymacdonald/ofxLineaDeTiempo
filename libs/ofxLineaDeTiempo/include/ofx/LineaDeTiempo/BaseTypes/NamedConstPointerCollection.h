d4c3a16fd9adb9592a329694fe3a71c085e0b4fb
Roy Macdonald <macdonald.roy@gmail.com>
2020-03-27 21:59:29 -0300

    Fixed missing return in NamedConstPointerCollection

--- libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/BaseTypes/NamedConstPointerCollection.h
+++ libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/BaseTypes/NamedConstPointerCollection.h
@@ -19,11 +19,13 @@ class NamedConstPointerCollection
 {
 public:
 	
-//	static_assert(std::is_base_of<DOM::Node<T>, T>::value,
-//				  "NamedConstPointerCollection invalid templated type. It must be or inherit from ofx::DOM::Node");
+	//	static_assert(std::is_base_of<DOM::Node<T>, T>::value,
+	//				  "NamedConstPointerCollection invalid templated type. It must be or inherit from ofx::DOM::Node");
 	
 	
 	///\brief get the element  specified index using [index] . This function will not check if the index is whitin bounds so you must make sure it is
+	NamedConstPointerCollection();
+	
 	const T* operator[] (size_t index) const;
 	T* operator[] (size_t index);
 	
@@ -40,7 +42,7 @@ public:
 	const T* at(const std::string& id) const;
 	T* at(const std::string& id);
 	
-		
+	
 	
 	const std::vector<T*> & getCollection();
 	const std::vector<const T*> & getCollection() const;
@@ -58,7 +60,7 @@ public:
 	bool has(T* t);
 	
 	void clear();
-
+	
 	void forceReindexing();
 	
 	std::string makeUniqueName(std::string name, const std::string& defaultName);
@@ -69,35 +71,39 @@ protected:
 	std::vector<const T*> _constElements;
 	std::map<std::string, T*> _namedElements;
 	
-
+	
 };
-	
-	
 
 template<typename T>
-	const T* NamedConstPointerCollection<T>::operator[] (size_t index) const
-	{
-		return _constElements[index];
-	}
+NamedConstPointerCollection<T>::NamedConstPointerCollection()
+{
+	clear();
+}
 
 template<typename T>
-	T* NamedConstPointerCollection<T>::operator[] (size_t index)
-	{
-		return _elements[index];
-	}
-	
+const T* NamedConstPointerCollection<T>::operator[] (size_t index) const
+{
+	return _constElements[index];
+}
 
 template<typename T>
-	const T* NamedConstPointerCollection<T>::operator[] (const std::string& name) const
-	{
-		return this->at(name);
-	}
+T* NamedConstPointerCollection<T>::operator[] (size_t index)
+{
+	return _elements[index];
+}
+
 
 template<typename T>
-	T* NamedConstPointerCollection<T>::operator[] (const std::string& name)
-	{
-		return this->at(name);
-	}
+const T* NamedConstPointerCollection<T>::operator[] (const std::string& name) const
+{
+	return this->at(name);
+}
+
+template<typename T>
+T* NamedConstPointerCollection<T>::operator[] (const std::string& name)
+{
+	return this->at(name);
+}
 template<typename T>
 const T* NamedConstPointerCollection<T>::at(size_t index) const
 {
@@ -109,7 +115,7 @@ template<typename T>
 T* NamedConstPointerCollection<T>::at(size_t index)
 {
 	if(index < _elements.size()) return _elements[index];
-		
+	
 	return nullptr;
 }
 
@@ -127,130 +133,130 @@ T* NamedConstPointerCollection<T>::at(const std::string& id)
 }
 
 template<typename T>
-	const std::vector<T*> & NamedConstPointerCollection<T>::getCollection()
-	{
-		return _elements;
-	}
+const std::vector<T*> & NamedConstPointerCollection<T>::getCollection()
+{
+	return _elements;
+}
 
 template<typename T>
-	const std::vector<const T*> & NamedConstPointerCollection<T>::getCollection() const
-	{
-		return _constElements;
+const std::vector<const T*> & NamedConstPointerCollection<T>::getCollection() const
+{
+	return _constElements;
+}
+
+
+template<typename T>
+bool NamedConstPointerCollection<T>::add(T* t)
+{
+	if(_namedElements.count(t->getId())){
+		ofLogError("NamedConstPointerCollection<T>::add") << "fail. There is already another item with the same name. Not adding it";
+		return false;
 	}
 	
+	if(this->has(t))
+	{
+		ofLogError("NamedConstPointerCollection<T>::add") << "fail. This item is already part of the collection. Not adding it " << t->getId();
+		return false;
+	}
+	
+	_namedElements[t->getId()] = t;
+	_elements.push_back(t);
+	_constElements.push_back(t);
+	return true;
+}
+
 
 template<typename T>
-	bool NamedConstPointerCollection<T>::add(T* t)
+bool NamedConstPointerCollection<T>::remove(const std::string& id)
+{
+	return remove(at(id));
+}
+
+template<typename T>
+bool NamedConstPointerCollection<T>::remove(T* t)
+{
+	if(t == nullptr) return  false;
+	
+	for(size_t i = 0; i < _elements.size(); i++)
 	{
-		if(_namedElements.count(t->getId())){
-			ofLogError("NamedConstPointerCollection<T>::add") << "fail. There is already another item with the same name. Not adding it";
-			return false;
-		}
-		
-		if(this->has(t))
+		if(_elements[i] == t)
 		{
-			ofLogError("NamedConstPointerCollection<T>::add") << "fail. This item is already part of the collection. Not adding it " << t->getId();
+			
+			return remove(i);
+			
+		}
+	}
+	return false;
+	
+}
+
+template<typename T>
+bool NamedConstPointerCollection<T>::remove(size_t index)
+{
+	if(index < _elements.size())
+	{
+		
+		if(_elements[index] == nullptr)
+		{
+			ofLogError("NamedConstPointerCollection<T>::remove(T* t)") << "Something went wrong. The element at the specified index is nullptr. Try force rebuilding";
 			return false;
 		}
 		
-		_namedElements[t->getId()] = t;
-		_elements.push_back(t);
-		_constElements.push_back(t);
+		const T* ct = _elements[index];
+		
+		if(_constElements[index] != ct)
+		{
+			ofLogError("NamedConstPointerCollection<T>::remove(T* t)") << "Something went wrong. The internal collections dont match. Try force rebuilding";
+			return false;
+		}
+		
+		if( _namedElements.count(_elements[index]->getId()) == 0)
+		{
+			ofLogError("NamedConstPointerCollection<T>::remove(T* t)") << "Something went wrong. The internal collections dont match. Try force rebuilding";
+			return false;
+		}
+		_namedElements.erase(_elements[index]->getId());
+		_elements.erase(_elements.begin() + index);
+		_constElements.erase(_constElements.begin() + index);
 		return true;
 	}
+	return false;
+}
+
+
+template<typename T>
+size_t NamedConstPointerCollection<T>::size() const
+{
+	return _elements.size();
+}
+
+
+template<typename T>
+bool NamedConstPointerCollection<T>::has(T* t)
+{
+	return (std::find_if(_elements.begin(),
+						 _elements.end(),
+						 [&](T* element)
+						 {
+		return t == element;
+	}
+						 ) != _elements.end());
+}
+
+
+template<typename T>
+void NamedConstPointerCollection<T>::clear()
+{
 	
-
-template<typename T>
-	bool NamedConstPointerCollection<T>::remove(const std::string& id)
-	{
-		return remove(at(id));
-	}
-
-template<typename T>
-	bool NamedConstPointerCollection<T>::remove(T* t)
-	{
-		if(t == nullptr) return  false;
-		
-		for(size_t i = 0; i < _elements.size(); i++)
-		{
-			if(_elements[i] == t)
-			{
-				
-				return remove(i);
-				
-			}
-		}
-			return false;
-			
-	}
-
-template<typename T>
-	bool NamedConstPointerCollection<T>::remove(size_t index)
-	{
-		if(index < _elements.size())
-		{
-			
-			if(_elements[index] == nullptr)
-			{
-				ofLogError("NamedConstPointerCollection<T>::remove(T* t)") << "Something went wrong. The element at the specified index is nullptr. Try force rebuilding";
-				return false;
-			}
-			
-			const T* ct = _elements[index];
-			
-			if(_constElements[index] != ct)
-			{
-				ofLogError("NamedConstPointerCollection<T>::remove(T* t)") << "Something went wrong. The internal collections dont match. Try force rebuilding";
-				return false;
-			}
-			
-			if( _namedElements.count(_elements[index]->getId()) == 0)
-			{
-				ofLogError("NamedConstPointerCollection<T>::remove(T* t)") << "Something went wrong. The internal collections dont match. Try force rebuilding";
-				return false;
-			}
-			_namedElements.erase(_elements[index]->getId());
-			_elements.erase(_elements.begin() + index);
-			_constElements.erase(_constElements.begin() + index);
-			return true;
-		}
-			return false;
-	}
-	
-
-template<typename T>
-	size_t NamedConstPointerCollection<T>::size() const
-	{
-		_elements.size();
-	}
-	
-
-template<typename T>
-	bool NamedConstPointerCollection<T>::has(T* t)
-	{
-		return (std::find_if(_elements.begin(),
-							_elements.end(),
-							[&](T* element)
-								{
-									return t == element;
-								}
-							 ) != _elements.end());
-	}
-	
-
-template<typename T>
-	void NamedConstPointerCollection<T>::clear()
-	{
-
-		_elements.clear();
-		_constElements.clear();
-		_namedElements.clear();
-	}
+	_elements.clear();
+	_constElements.clear();
+	_namedElements.clear();
+}
 
 template<typename T>
 void NamedConstPointerCollection<T>::forceReindexing()
 {
-
+	
 	
 	// remove any pointer that is invalid
 	ofRemove(_elements, [](T* e){return !e;});
