e3bf01b7cb80b6fd7342807221a50e25adf8ba91
Roy Macdonald <macdonald.roy@gmail.com>
2020-04-03 21:43:47 -0300

    Added double and long double to TimedData and KeyframedData template classes

--- libs/ofxLineaDeTiempo/src/Data/TimedData_impl.cpp
+++ libs/ofxLineaDeTiempo/src/Data/TimedData_impl.cpp
@@ -13,39 +13,6 @@ namespace LineaDeTiempo {
 
 
 
-
-//#define ENABLE_GLM_QUAT_MAT_TIMED_DATA
-//#define ENABLE_OF_RECTANGLE_TIMED_DATA
-//#define ENABLE_OF_COLOR_DOUBLE_TIMED_DATA
-//#define ENABLE_LONG_DOUBLE_TIMED_DATA
-
-
-//
-//#ifdef ENABLE_OF_RECTANGLE_TIMED_DATA
-//template class TimedData_<ofRectangle>;
-//#endif
-//
-//template class TimedData_<ofColor_<char>>;
-//template class TimedData_<ofColor_<unsigned char>>;
-//template class TimedData_<ofColor_<short>>;
-//template class TimedData_<ofColor_<unsigned short>>;
-//template class TimedData_<ofColor_<int>>;
-//template class TimedData_<ofColor_<unsigned int>>;
-//template class TimedData_<ofColor_<long>>;
-//template class TimedData_<ofColor_<unsigned long>>;
-//template class TimedData_<ofColor_<float>>;
-//#ifdef ENABLE_OF_COLOR_DOUBLE_TIMED_DATA
-//template class TimedData_<ofColor_<double>>;
-//#endif
-//
-//template class TimedData_<glm::vec2>;
-//template class TimedData_<glm::vec3>;
-//template class TimedData_<glm::vec4>;
-//#ifdef ENABLE_GLM_QUAT_MAT_TIMED_DATA
-//template class TimedData_<glm::quat>;
-//template class TimedData_<glm::mat4>;
-//#endif
-//
 template class TimedData_<char>;
 template class TimedData_<unsigned char>;
 template class TimedData_<signed char>;
@@ -58,43 +25,9 @@ template class TimedData_<unsigned long>;
 template class TimedData_<long long>;
 template class TimedData_<unsigned long long>;
 template class TimedData_<float>;
-//template class TimedData_<double>;
-//template class TimedData_<long double>;
+template class TimedData_<double>;
+template class TimedData_<long double>;
 
-//template class TimedData_<double>;
-//#ifdef ENABLE_LONG_DOUBLE_TIMED_DATA
-//template class TimedData_<long double>;
-//#endif
-
-//
-////-----------------------------------------------------------------------------
-//#ifdef ENABLE_OF_RECTANGLE_TIMED_DATA
-//template class KeyframedData_<ofRectangle>;
-//#endif
-//template class KeyframedData_<ofColor_<char>>;
-//template class KeyframedData_<ofColor_<unsigned char>>;
-//template class KeyframedData_<ofColor_<short>>;
-//template class KeyframedData_<ofColor_<unsigned short>>;
-//template class KeyframedData_<ofColor_<int>>;
-//template class KeyframedData_<ofColor_<unsigned int>>;
-//template class KeyframedData_<ofColor_<long>>;
-//template class KeyframedData_<ofColor_<unsigned long>>;
-//template class KeyframedData_<ofColor_<float>>;
-//#ifdef ENABLE_OF_COLOR_DOUBLE_TIMED_DATA
-//template class KeyframedData_<ofColor_<double>>;
-//#endif
-//
-//template class KeyframedData_<glm::vec2>;
-//template class KeyframedData_<glm::vec3>;
-//template class KeyframedData_<glm::vec4>;
-//
-//#ifdef ENABLE_GLM_QUAT_MAT_TIMED_DATA
-//template class KeyframedData_<glm::quat>;
-//template class KeyframedData_<glm::mat4>;
-//#endif
-//
-//
-//
 template class KeyframedData_<char>;
 template class KeyframedData_<unsigned char>;
 template class KeyframedData_<signed char>;
@@ -107,24 +40,9 @@ template class KeyframedData_<unsigned long>;
 template class KeyframedData_<long long>;
 template class KeyframedData_<unsigned long long>;
 template class KeyframedData_<float>;
-//template class KeyframedData_<double>;
-//template class KeyframedData_<long double>;
+template class KeyframedData_<double>;
+template class KeyframedData_<long double>;
 	
-// template class KeyframedData_<char>;
-// template class KeyframedData_<unsigned char>;
-// template class KeyframedData_<short>;
-// template class KeyframedData_<unsigned short>;
-// template class KeyframedData_<int>;
-// template class KeyframedData_<unsigned int>;
-// template class KeyframedData_<long>;
-// template class KeyframedData_<unsigned long>;
-// template class KeyframedData_<float>;
-//template class KeyframedData_<double>;
-//#ifdef ENABLE_LONG_DOUBLE_TIMED_DATA
-//template class KeyframedData_<long double>;
-//#endif
-
-
 
 
 } } // ofx::LineaDeTiempo
