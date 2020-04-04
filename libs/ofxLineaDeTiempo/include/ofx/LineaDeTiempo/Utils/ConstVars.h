b72934fd92535d45a0735d57edf6193d2c6ae207
Roy Macdonald <macdonald.roy@gmail.com>
2020-04-03 21:20:51 -0300

    Added a few more const vars

--- libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/Utils/ConstVars.h
+++ libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/Utils/ConstVars.h
@@ -8,30 +8,37 @@
 #pragma once
 #include "ofColor.h"
 #include <vector>
-
 namespace ofx {
 namespace LineaDeTiempo {
 
+static const ofColor DefaultTextColor = ofColor(220);
+
 //BaseTrackView
-static const ofColor TrackBackgroundColor = ofColor(80);
+static const ofColor TrackBackgroundColor = ofColor(40);
 static const ofColor TrackEdgeColor = ofColor(120);
-static const float TrackInitialHeight = 150.0;
-
-//ofColor BaseTrackView::backgroundColor = ofColor(80);
-//ofColor BaseTrackView::edgeColor = ofColor(120);
-//const float BaseTrackView::initialHeight = 150;
-
+static const float   TrackInitialHeight = 150.0;
 
 //KeyframeView
-static const float DefaultKeyframeSize = 15.0;
-//float KeyframeView::defaultKeyframeSize = 15;
+static const float DefaultKeyframeSize = 5.0;
 
 //RegionView
 static float RegionViewHeaderHeight = 18.f;
-//float RegionView::headerHeight = 18.f;
+static const ofColor RegionBackgroundColor = ofColor(80);
+
+//HeaderView
+
+static float HeaderViewIndent = 25.f;
+
+
+//TimeRuler
+
+static float TimeRulerInitialHeight = 30.f;
+
+//Names
 
 static const std::string DimensionNames [4] = { "x", "y", "z", "w" };
 
+static const std::string ColorChannelNames [4] = { "r", "g", "b", "a" };
 
 static std::string GetNameForDimension(const size_t & dim, const size_t & totalDims)
 {
