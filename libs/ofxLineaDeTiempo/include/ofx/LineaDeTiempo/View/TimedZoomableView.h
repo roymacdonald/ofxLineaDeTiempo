6f21a397f400b2cca07c839acd0705e0d456f263
Roy Macdonald <macdonald.roy@gmail.com>
2020-07-28 20:58:06 -0400

    Added TimedZoomableView class files. This might replace ZoomablePanel

---
+++ libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/View/TimedZoomableView.h
@@ -0,0 +1,13 @@
+//
+//  TimedZoomableView.hpp
+//  oscGuiTimeline
+//
+//  Created by Roy Macdonald on 7/28/20.
+//
+
+#ifndef TimedZoomableView_hpp
+#define TimedZoomableView_hpp
+
+#include <stdio.h>
+
+#endif /* TimedZoomableView_hpp */
---
+++ libs/ofxLineaDeTiempo/src/View/TimedZoomableView.cpp
@@ -0,0 +1,8 @@
+//
+//  TimedZoomableView.cpp
+//  oscGuiTimeline
+//
+//  Created by Roy Macdonald on 7/28/20.
+//
+
+#include "TimedZoomableView.h"
