8bf7255f2897dd12b924889bc8e3605c97fd33ad
Roy Macdonald <macdonald.roy@gmail.com>
2020-08-08 17:58:28 -0400

    Disabled ofxMUI::MUI as it was not being used.

--- libs/ofxMUI/include/ofx/MUI/MUI.h
+++ libs/ofxMUI/include/ofx/MUI/MUI.h
@@ -1,54 +1,63 @@
+////
+//// Copyright (c) 2009 Christopher Baker <https://christopherbaker.net>
+////
+//// SPDX-License-Identifier: MIT
+////
 //
-// Copyright (c) 2009 Christopher Baker <https://christopherbaker.net>
 //
-// SPDX-License-Identifier: MIT
+//#pragma once
 //
-
-
-#pragma once
-	
-
-#include "ofx/DOM/Document.h"
-
-
-namespace ofx {
-namespace MUI {
-
-
-class Styles;
-
-
-class MUI: public DOM::Document
-{
-public:
-    enum Scale
-    {
-        SMALL,
-        MEDIUM,
-        LARGE,
-        X
-    };
-
-    MUI(const DOM::DocumentSettings& settings);
-    virtual ~MUI();
-
-
-    std::shared_ptr<Styles> getDocumentStyles() const;
-    
-    void setDocumentStyles(std::shared_ptr<Styles> documentStyles);
-
-protected:
-    mutable std::shared_ptr<Styles> _documentStyles = nullptr;
-
-};
-
-
-NLOHMANN_JSON_SERIALIZE_ENUM( MUI::Scale, {
-    { MUI::Scale::SMALL, "SMALL" },
-    { MUI::Scale::MEDIUM, "MEDIUM"},
-    { MUI::Scale::LARGE, "LARGE"},
-    { MUI::Scale::X, "X"}
-})
-
-
-} } // ofx::MUI
+//
+//#include "ofx/DOM/Document.h"
+//
+//
+//namespace ofx {
+//namespace MUI {
+//
+//
+////class Styles;
+//
+//
+//class MUI: public DOM::Document
+//{
+//public:
+////    enum Scale
+////    {
+////        SMALL,
+////        MEDIUM,
+////        LARGE,
+////        X
+////    };
+//
+//	MUI(const DOM::DocumentSettings& settings)
+//	:DOM::Document(settings)
+//	{
+//
+//	}
+//
+//
+//	virtual ~MUI()
+//	{
+//
+//	}
+//
+////
+////    std::shared_ptr<Styles> getDocumentStyles() const;
+////
+////    void setDocumentStyles(std::shared_ptr<Styles> documentStyles);
+////
+////protected:
+////    mutable std::shared_ptr<Styles> _documentStyles = nullptr;
+//
+//};
+//
+//
+////NLOHMANN_JSON_SERIALIZE_ENUM( MUI::Scale, {
+////    { MUI::Scale::SMALL, "SMALL" },
+////    { MUI::Scale::MEDIUM, "MEDIUM"},
+////    { MUI::Scale::LARGE, "LARGE"},
+////    { MUI::Scale::X, "X"}
+////})
+//
+//
+//} } // ofx::MUI
--- libs/ofxMUI/src/MUI.cpp
+++ libs/ofxMUI/src/MUI.cpp
@@ -1,43 +1,28 @@
+////
+//// Copyright (c) 2009 Christopher Baker <https://christopherbaker.net>
+////
+//// SPDX-License-Identifier: MIT
+////
 //
-// Copyright (c) 2009 Christopher Baker <https://christopherbaker.net>
 //
-// SPDX-License-Identifier: MIT
+//#include "ofx/MUI/MUI.h"
+//#include "ofx/MUI/Styles.h"
 //
-
-
-#include "ofx/MUI/MUI.h"
-#include "ofx/MUI/Styles.h"
-
-
-namespace ofx {
-namespace MUI {
-
-
-MUI::MUI(const DOM::DocumentSettings& settings): DOM::Document(settings)
-{
-}
-
-
-MUI::~MUI()
-{
-}
-
-
-std::shared_ptr<Styles> MUI::getDocumentStyles() const
-{
-    if (_documentStyles == nullptr)
-    {
-        _documentStyles = std::make_shared<Styles>("Document Styles");
-    }
-
-    return _documentStyles;
-}
-
-
-void MUI::setDocumentStyles(std::shared_ptr<Styles> documentStyles)
-{
-    _documentStyles = documentStyles;
-}
-
-
-} } // namespace ofx::MUI
+//
+//namespace ofx {
+//namespace MUI {
+//
+//
+//MUI::MUI(const DOM::DocumentSettings& settings): DOM::Document(settings)
+//{
+//}
+//
+//
+//MUI::~MUI()
+//{
+//}
+//
+//
+//
+//
+//} } // namespace ofx::MUI
