bec254148d29942989be0c92e2d3bbfb661adeca
Roy Macdonald <macdonald.roy@gmail.com>
2020-09-01 12:59:31 -0400

    Added inline docummentation for ConstVars

--- libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/Utils/ConstVars.h
+++ libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/Utils/ConstVars.h
@@ -15,24 +15,41 @@ namespace LineaDeTiempo {
 struct ConstVars
 {
 	
+	/** @name Colors
+	 *	Most of the colors are defined through the document styles, yet there are a few things that looked better if their color was set individually.
+	 */
+	///@{
 	
+	///\brief The background color of the main view (panel). This color will be mostly shown when there is nothing in that area, as most of the UI elements have solid colors
 	static ofParameter<ofColor> PanelBackgroundColor;
-	static ofParameter<ofColor> TrackBackgroundColor  ;
-	static ofParameter<ofColor> TrackEdgeColor        ;
-	static ofParameter<ofColor> TooltipTextColor;
-	static ofParameter<ofColor> TooltipBackgroundColor;
-	static ofParameter<ofColor> TooltipBorderColor;
-
+	
+	///\brief The background color of each track.
+	/// The idea is that it is slightly different from the panel background color so it is easily perceived what is a track and what is not.
+	static ofParameter<ofColor> TrackBackgroundColor;
+	
+	///\brief The edge color of each track
+	static ofParameter<ofColor> TrackEdgeColor      ;
 	static ofColor SelectedColor;
 	
-	static ofParameter<float> TrackInitialHeight      ;
-	static ofParameter<float> DefaultKeyframeSize     ;
-	static ofParameter<float> ViewTopHeaderHeight     ;
-	static ofParameter<float> HeaderViewIndent        ;
-	static ofParameter<float> TimeRulerInitialHeight  ;
-	static ofParameter<float> PlayheadTriangleSize    ;
-	static ofParameter<float> PlayheadWidth           ;
 	
+	
+	///@}
+	/** @name ToolTip Options
+	 *  The Tooltip is a small text box that shows up as an aid for the user when it hovers the mouse over elements that have it enabled.
+	 *  The tooltip shows at the mouse position, thus its name.
+	 */
+	///@{
+	
+	///\brief The Tooltip's text color
+	static ofParameter<ofColor> TooltipTextColor;
+	
+	///\brief The Tooltip's bounding box background color
+	static ofParameter<ofColor> TooltipBackgroundColor;
+	
+	///\brief The Tooltip's bounding box border color
+	static ofParameter<ofColor> TooltipBorderColor;
+	
+
 	///\brief How much time the pointer needs to be without moving over an element with tooltips in order to show thet tooltip
 	/// units: milliseconds
 	static ofParameter<uint64_t> tooltipDelay;
@@ -42,9 +59,41 @@ struct ConstVars
 	
 	///\brief Show tooltips on elements that have this feature implemented
 	static ofParameter<bool> showTooltips;
+
+	///@}
 	
 	
-	///\brief get all the ConstVars parameters inside an ofParameterGroup
+	/** @name Sizes
+	 *	Default or initial sizes of some of the UI elements.
+	 */
+	///@{
+	
+	///\brief This is the initial track height. Even when this is used for the initial height it is used as the tracks height changes as a point of reference for some calculations
+	static ofParameter<float> TrackInitialHeight      ;
+	
+	///\brief The width and height of the drawn keyframes; circles
+	static ofParameter<float> DefaultKeyframeSize     ;
+	
+	///\brief The height of the top header that some elements have.
+	static ofParameter<float> ViewTopHeaderHeight     ;
+	
+	///\brief The left indent added to the tracks headers when these belong to a group
+	static ofParameter<float> HeaderViewIndent        ;
+	
+	///\brief The initial height of the time ruler. so far this does not change dynamically
+	static ofParameter<float> TimeRulerInitialHeight  ;
+	
+	///\brief The width of the line drawn at the playhead position.
+	static ofParameter<float> PlayheadWidth           ;
+	
+	///\brief The size of the triangle drawn at the top of the playhead line.
+	static ofParameter<float> PlayheadTriangleSize    ;
+	
+	
+	///@}
+	
+	
+	///\brief get an ofParameterGroup containing all the ConstVars parameters
 	static ofParameterGroup& getParameters();
 	
 };
