e86ee821c11618a05ee6a30d8e460c2345bc6bb0
Roy Macdonald <macdonald.roy@gmail.com>
2023-12-28 16:44:44 +0400

    changed  std::enable_if< not to  std::enable_if<!

--- libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/Utils/ParamHelper.h
+++ libs/ofxLineaDeTiempo/include/ofx/LineaDeTiempo/Utils/ParamHelper.h
@@ -21,14 +21,14 @@ getUnnormalized(const float&  val, const size_t& dim, const ofParameter<DataType
 }
 
 template<typename DataType>
-typename std::enable_if<not is_multi_dim_param<DataType>::value, DataType>::type
+typename std::enable_if<! is_multi_dim_param<DataType>::value, DataType>::type
 getUnnormalized(const float&  val, const size_t& dim, const ofParameter<DataType>& param){
 	return ofMap(val, 0, 1, (float)param.getMin(), param.getMax(), true);
 }
 
 
 template<typename DataType>
-typename std::enable_if<not is_multi_dim_param<DataType>::value, float>::type
+typename std::enable_if<! is_multi_dim_param<DataType>::value, float>::type
 getNormalized(const DataType& val, const size_t& dim, const ofParameter<DataType>& param)
 {
 	return ofMap((float)val, param.getMin(), param.getMax(), 0, 1, true);
